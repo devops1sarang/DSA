279111a9492c645aea8f10d0f39929d78adc2e69
devops1sarang <70004382+devops1sarang@users.noreply.github.com>
2022-09-12 11:51:15 +0530

    Add files via upload

---
+++ src/main/java/com/sarangtech/demo/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.sarangtech.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping(value = "/get/{name}")
+    public ResponseEntity<?> getName(@PathVariable("name") String name) {
+        return new ResponseEntity<>("hi " + name, HttpStatus.OK);
+    }
+}
